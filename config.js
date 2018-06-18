fcac48caeb361dda7368bfd88b0622d424ec7402
Thomas Rix <trix@dr.dk>
2018-06-18 07:53:12 +0200

    Tiny css change to a tag in textfield

--- config.js
+++ config.js
@@ -9,12 +9,14 @@ let config = {
     staging: {
         DEBUGGING:true,
         ASSETS_PATH:JSON.stringify('https://www.dr.dk/tjenester/visuel/staging/video-scroller-mord/assets/'),
-        SCROLL_IMAGE_PATH:JSON.stringify('https://downol.dr.dk/download/nyheder/2018/mord-scroller/images/')
+        SCROLL_IMAGE_PATH:JSON.stringify('https://www.dr.dk/tjenester/visuel/scroll-video/danmarks-drabeligste-rute/')
+        // SCROLL_IMAGE_PATH:JSON.stringify('https://downol.dr.dk/download/nyheder/2018/mord-scroller/images/')
     },
     deploy: {
         DEBUGGING:false,
         ASSETS_PATH:JSON.stringify('https://www.dr.dk/tjenester/netdoks/Holmbladsgaderuten/video-scroller-mord/assets/'),
-        SCROLL_IMAGE_PATH:JSON.stringify('https://downol.dr.dk/download/nyheder/2018/mord-scroller/images/')
+        SCROLL_IMAGE_PATH:JSON.stringify('https://www.dr.dk/tjenester/visuel/scroll-video/danmarks-drabeligste-rute/')
+        // SCROLL_IMAGE_PATH:JSON.stringify('https://downol.dr.dk/download/nyheder/2018/mord-scroller/images/')
     }
 }
 module.exports = config;
